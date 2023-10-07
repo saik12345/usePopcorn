91eb877c5c59154d0117b429973be9097cd4106d
saik12345 <111682963+saik12345@users.noreply.github.com>
2023-10-07 18:39:13 +0530

    Add files via upload

---
+++ src/useMovies.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+const KEY = "4eedba21";
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      callback?.();
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!res.ok) throw new Error("Something went wrong!!");
+
+          const data = await res.json();
+          // console.log(data);
+          if (data.Response === "False") throw new Error("Movie not found!!");
+          setMovies(data.Search);
+        } catch (error) {
+          console.log(error.message);
+          if (error.name !== "AbortError") setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 2) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      //   handleCloseMovie();
+      fetchMovies();
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
