58de3e302314dc589180a0dce19f27ac4ce2f382
saik12345 <111682963+saik12345@users.noreply.github.com>
2023-10-08 08:59:00 +0530

    Update useLocalStorageState.js

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
-    return JSON.parse(storedValue);
+    return storedValue?JSON.parse(storedValue):initialSate;
   });
 
   useEffect(
